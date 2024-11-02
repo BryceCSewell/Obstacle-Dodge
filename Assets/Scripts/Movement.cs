f6ffcc7969e1401de0cacfe6f428853f04a0fb25
BryceCSewell <bryce.sewell7567@gmail.com>
2024-11-02 11:57:22 -0500

    Dump of all assets from creation of application

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,12 +7,15 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xValue = Input.GetAxis("Horizontal");
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, yValue, zValue);
     }
 }
